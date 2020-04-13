77505fa95a19d2c01e992e39d67febd5da1f27f3
Ishpuneet Singh <ishpuneetsingh007@gmail.com>
2020-04-12 23:44:18 -0400

    Update windowsserver.ps1

--- windowsserver.ps1
+++ windowsserver.ps1
@@ -8,11 +8,12 @@
 
 #PowerShell Script to delete folder/files on the windows server
 
-Start-Transcript -Path "C:\Users\ishba\Downloads\Test\trans.txt" -noclobber -append
+Start-Transcript -Path "C:\Users\ishba\Downloads\Test\tran.txt" -noclobber -append
 
 If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
  {
   Echo "This script needs to be run As Admin"
+ Stop-Transcript
   Break
  }
 else {
@@ -24,12 +25,14 @@ if ( Test-Path C:\Windows ) {
     echo "Directory Exists"
 } else {
     echo "Directory doesn't Exists"
+Stop-Transcript
  Break
 }
 
 
 If ((Get-ChildItem -Force C:\Users\ishba\Downloads\Test\test1) -eq $Null) {
     "the Folder is empty"
+Stop-Transcript
 Break
 }
 else {
@@ -47,8 +50,7 @@ Remove-Item   -Path $Dir1 -recurse
 Get-ChildItem -Path $Dir2  -Include * -File -Recurse | foreach { $_.Delete()}   
 
 echo "The files have been deleted successfully" 
-
+Stop-Transcript
 exit
-
 
 
