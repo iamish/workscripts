8099e0e8d041ff76e71f9e11289a48a07f59a2d6
Ishpuneet Singh <ishpuneetsingh007@gmail.com>
2020-04-14 14:25:58 -0400

    Update stagingserver.sh

--- stagingserver.sh
+++ stagingserver.sh
@@ -6,23 +6,30 @@
 #   Delete Folders on the server with on the fly cron
 #####################################################
 #!/bin/bash
+log_file="logg.txt"
+exec &> >(tee -a "$log_file")
+echo "**********************************************"
+echo -e  "***********\nNew Log Entry\n************"
+echo "**********************************************"
+
+
+find . -name "*.log" -print -mtime +1 -type f -exec rm {} \;
+#find . -name "*.txt" -print -mtime +1 -type f -delete
 if [[ $EUID -ne 0 ]]; then
    echo "This script must be run as root"
    exit 1
    else
  echo " You are running this scirpt as root user"
 fi
-
-
 DIR="/home/ishpuneetsingh007/test"
+DIRR="/home/ishpuneetsingh007/test/"
 if [ -d "$DIR" ]; then
   # Control will enter here if $DIRECTORY exists.
    echo " The directory exists"
-else 
+else
 echo " The directory doesn't exist"
 exit
 fi
-
 # init
 # look for empty dir
 if [ "$(ls -A $DIR)" ]; then
@@ -31,5 +38,14 @@ else
     echo "$DIR is Empty"
 exit
 fi
-echo "**************************"
-
+####################################################################################
+#####Delete files timestamped 24 hours ago leaving the directory structure intact
+##
+#
+#find "$DIRR" -name "*.log" -print -type f -exec rm {} \;
+#find "$DIRR" -print -type f -exec rm {} \;
+find "$DIRR" -print -mtime +1 -type f -exec rm {} \;
+ 
+echo "Files Deleted"
+echo "**************************"     
+exit 0
