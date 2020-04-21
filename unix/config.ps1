ffd025ed49960ad2e620cf2799d30c65be25d80a
Ishpuneet Singh <ishpuneetsingh007@gmail.com>
2020-04-21 18:40:46 -0400

    Update config.ps1

--- unix/config.ps1
+++ unix/config.ps1
@@ -1,2 +1,2 @@
-$Dir1 = "C:\Users\ish\Desktop\Test\test3\"
-$Dir2 = "C:\Users\ish\Desktop\Test\logfile1.txt"
+$Dir2 = "C:\Users\ish\Desktop\Test\test3\"
+$Dir1 = "C:\Users\ish\Desktop\Test\logfile1.txt"
